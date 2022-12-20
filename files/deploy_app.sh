5c893ba9a4320dc58354126a761a6c1b580aeb5f
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2022-12-20 11:15:58 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Hope you are enjoying the Workshop so far!!.
+  Welcome to ${PREFIX}'s app. Hope you are enjoying the demo so far!!.
   <!-- END -->
 
   </div>
