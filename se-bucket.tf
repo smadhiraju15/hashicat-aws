ab3ecdc6229f106458995a7fe0a23082edccea10
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2022-12-23 15:51:34 +0530

    Update se-bucket.tf

--- se-bucket.tf
+++ se-bucket.tf
@@ -1,4 +1,6 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/HDFC-workshop/s3-bucket/aws"
-  version = "3.6.0"
+  source  = "app.terraform.io/smadhiraju-org/s3-bucket/aws"
+  version = "3.5.0"
+  
+  bucket_prefix = "sandeep"
 }
