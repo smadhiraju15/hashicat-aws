941f3a8808311264e4af98b99125d13e7510753e
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2023-08-10 06:24:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
+    Department = "app-dev-team"
     Billable = "true"
   }
 }
